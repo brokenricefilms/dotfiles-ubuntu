db515b5333002979b0047f8e3c2647e118a1f756
Broken Rice Films <brokenricefilms@gmail.com>
2024-07-09 10:57:36 +0700

    auto commit

---
+++ .bashrc
@@ -0,0 +1,106 @@
+HISTCONTROL=ignoreboth
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=9999999999999
+HISTFILESIZE=9999999999999
+
+alias d="aria2c -x 16 -s 16 --seed-time=0 $args"
+alias dow="cd ~/Downloads"
+alias reload="source ~/.bashrc;tmux source ~/.tmux.conf"
+alias fd="fdfind"
+alias q="exit"
+alias ls="ls -al"
+alias l="ls -al"
+alias la="\ls -a"
+alias batcat="bat"
+alias ..="cd .."
+alias doc="cd ~/Documents"
+
+function update() {
+    cp ~/.bashrc ~/repos/brokenricefilms/dotfiles-ubuntu/
+    cp ~/.tmux.conf ~/repos/brokenricefilms/dotfiles-ubuntu/
+    cp ~/.gitconfig ~/repos/brokenricefilms/dotfiles-ubuntu/
+    cp -r ~/.config/nvim/ ~/repos/brokenricefilms/dotfiles-ubuntu/
+    cd ~/repos/brokenricefilms/dotfiles-ubuntu/
+    git add -A
+    git commit -m "auto commit"
+    git push
+    cd -
+    sudo apt update -y
+    sudo apt upgrade -y
+}
+
+function gitAutoCommit() {
+  git add -A
+  git commit -m 'auto commit'
+  git push
+}
+alias aa="gitAutoCommit"
+
+function fzfEditFile() {
+  if [ -z "$1" ]; then
+    FILE=$(fd --hidden --type file . | fzf --preview 'bat --theme=GitHub --color=always --style=numbers --line-range=:501 {}' &)
+
+    if [ -n "$FILE" ]; then
+      nvim "$FILE"
+    fi
+  else
+    nvim "$1"
+  fi
+}
+alias e="fzfEditFile"
+alias e.="nvim ."
+alias ee="cd $HOME; fzfEditFile"
+
+function fzfChangeDirectory() {
+  DIR=$1
+
+  fzfDir() {
+    DIR=$(fd --hidden --type directory . | fzf --preview $'echo {} | tr -d \'()\' | awk \'{printf "%s ",  $2} {print  $1}\' | xargs -r exa --long --all --icons --color=never' &)
+    cd "$DIR"
+    ls
+  }
+
+  if [ -z "$1" ]; then
+    if [[ -n $DIR ]]; then
+      cd $DIR &>/dev/null
+      ERROR=$?
+
+      if [[ ERROR -eq 1 ]]; then
+        echo "\"$1\" directory does not exist"
+        fzfDir
+      fi
+    else
+      fzfDir
+    fi
+  else
+    mkdir -p "$1"
+    cd "$1"
+  fi
+}
+alias c="fzfChangeDirectory"
+alias cc="cd $HOME; fzfChangeDirectory"
+
+function tmuxKillAllUnnameSession() {
+  tmux ls | awk '{print $1}' | grep -o '[0-9]\+' >/tmp/killAllUnnameTmuxSessionOutput.sh
+  sed -i 's/^/tmux kill-session -t /' /tmp/killAllUnnameTmuxSessionOutput.sh
+  chmod +x /tmp/killAllUnnameTmuxSessionOutput.sh
+  /tmp/killAllUnnameTmuxSessionOutput.sh
+}
+
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
+export PNPM_HOME="/home/master/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+
+eval "$(starship init bash)"
---
+++ nvim/init.lua
@@ -0,0 +1,162 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {
+        "stevearc/oil.nvim",
+        opts = {
+            skip_confirm_for_simple_edits = true,
+            keymaps = {
+                ["q"] = {
+                    callback = function()
+                        require("oil").close()
+                        vim.cmd("close")
+                    end,
+                },
+            },
+        },
+    },
+    {
+        "projekt0n/github-nvim-theme",
+        event = "VeryLazy",
+    },
+    { 
+        "nvim-lua/plenary.nvim", 
+        event = "VeryLazy",
+    },
+    {
+        "nvim-pack/nvim-spectre",
+        requires = { {'nvim-lua/plenary.nvim'} },
+        event = "VeryLazy",
+        cmd = "Spectre",
+        opts = { open_cmd = "noswapfile vnew" },
+        -- stylua: ignore
+        keys = {
+            { "<leader>r", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
+        },
+    },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        requires = { {'nvim-lua/plenary.nvim'} },
+        event = "VeryLazy",
+    }
+})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>;', builtin.commands, {})
+
+vim.keymap.set("i", "jk", "<Esc>")
+
+vim.opt.background = "light"
+vim.cmd("colorscheme github_light")
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.mouse = "a"
+vim.opt.swapfile = false
+vim.opt.completeopt = "menuone,noinsert,noselect"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.showmatch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.showbreak = "↪"
+vim.opt.scrolloff = 5
+vim.opt.termguicolors = true
+vim.opt.statusline = " %F %m %< %= %r %q"
+vim.opt.cursorline = true
+vim.opt.laststatus = 0
+vim.opt.winbar = "%=%F"
+
+vim.opt.hidden = true
+vim.opt.history = 100
+vim.opt.lazyredraw = true
+vim.opt.updatetime = 100
+vim.opt.undofile = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "*", "*zz")
+vim.keymap.set("n", "#", "#zz")
+
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+vim.keymap.set("n", "<Right>", "<C-w><")
+vim.keymap.set("n", "<Down>", "<C-w>-")
+vim.keymap.set("n", "<Up>", "<C-w>+")
+vim.keymap.set("n", "<Left>", "<C-w>>")
+
+vim.keymap.set("x", "<", "<gv")
+vim.keymap.set("x", ">", ">gv")
+
+vim.keymap.set("n", "gf", ":cd %:h<enter>:edit <cfile><enter>")
+
+vim.keymap.set("n", "<leader><space>", "<C-^>")
+
+vim.keymap.set("n", "<leader>k", "<C-w>s:e %:h<enter>", { silent = true })
+
+vim.keymap.set("n", "<leader>h", "<C-w>v:e %:h<enter>", { silent = true })
+
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = "*",
+})
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+    callback = function()
+        local ok, cl = pcall(vim.api.nvim_win_get_var, 0, "auto-cursorline")
+        if ok and cl then
+            vim.wo.cursorline = true
+            vim.api.nvim_win_del_var(0, "auto-cursorline")
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
+    callback = function()
+        local cl = vim.wo.cursorline
+        if cl then
+            vim.api.nvim_win_set_var(0, "auto-cursorline", cl)
+            vim.wo.cursorline = false
+        end
+    end,
+})
+
+-- Restore cursor position
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+    pattern = { "*" },
+    callback = function()
+        vim.api.nvim_exec('silent! normal! g`"zv', false)
+    end,
+})
